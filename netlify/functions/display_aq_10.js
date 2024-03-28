f78c03010e962be4031ab385831c112696a36827
Muhammad Ahsan Ali <91987110+M-Ahsan-Ali-001@users.noreply.github.com>
2024-03-28 09:34:12 -0700

    Update display_aq_10.js

--- netlify/functions/display_aq_10.js
+++ netlify/functions/display_aq_10.js
@@ -29,6 +29,9 @@ exports.handler = async function(event, context) {
         headers: {
             'Access-Control-Allow-Origin': '*', // replace '*' with your origin
             'Access-Control-Allow-Headers': 'Content-Type',
+                        "Access-Control-Allow-Origin": "*",
+          "    Access-Control-Allow-Methods":" GET, POST, PUT, DELETE",
+"Access-Control-Allow-Headers": "Authorization"
          
         }
     };
