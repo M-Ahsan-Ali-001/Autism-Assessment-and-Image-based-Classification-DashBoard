468ca6904e1baee1a41ee8fcf21877ca42b4a0de
Muhammad Ahsan Ali <91987110+M-Ahsan-Ali-001@users.noreply.github.com>
2024-03-28 12:32:26 -0700

    Update display_model.js

--- netlify/functions/display_model.js
+++ netlify/functions/display_model.js
@@ -4,48 +4,42 @@ const {   Employee } =  require('../../backend/Schemas/model_ins_schema.js');
 const mongoURI = "mongodb+srv://admin:admin123@atlascluster.f9crw3i.mongodb.net/usersDB";
 
 exports.handler = async function(event, context) {
-
-
-  
-
     await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-  try{
 
-    let id = JSON.parse(event.body).id
-    console.log("asdasdasd",id)
+    try {
+        let id = JSON.parse(event.body).id;
+        console.log("asdasdasd", id);
 
-    const doc =await Employee.collection.find({user_id:id}).toArray();
-      if(doc){
-        console.log("@*******",doc);
+        const doc = await Employee.collection.find({ user_id: id }).toArray();
+        if (doc) {
+            console.log("@*******", doc);
 
-
-  
-      }
-     
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify(doc),
-        headers: {
-            'Access-Control-Allow-Origin': '*', // replace '*' with your origin
-            'Access-Control-Allow-Headers': 'Content-Type',
-         
+            return {
+                statusCode: 200,
+                body: JSON.stringify(doc),
+                headers: {
+                    'Access-Control-Allow-Origin': '*', // Allow requests from any origin
+                    'Access-Control-Allow-Headers': 'Content-Type', // Specify allowed headers
+                }
+            };
+        } else {
+            return {
+                statusCode: 200,
+                body: "Error!",
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                }
+            };
         }
-    };
-
-  }
-
-  catch(err){
-    return {
-        statusCode: 200,
-        body: err,
-        headers: {
-            'Access-Control-Allow-Origin': '*', // replace '*' with your origin
-            'Access-Control-Allow-Headers': 'Content-Type',
-    
-        }
-    };
-
-  }
-  
-  };
+    } catch (err) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ error: err.message }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            }
+        };
+    }
+};
