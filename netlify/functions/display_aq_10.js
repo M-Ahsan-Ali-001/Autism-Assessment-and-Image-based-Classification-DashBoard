6ee9039589b21311ddfc773e275dc5bbf362dfc0
Muhammad Ahsan Ali <91987110+M-Ahsan-Ali-001@users.noreply.github.com>
2024-03-28 11:53:30 -0700

    Update display_aq_10.js

--- netlify/functions/display_aq_10.js
+++ netlify/functions/display_aq_10.js
@@ -26,6 +26,9 @@ exports.handler = async function(event, context) {
       return {
         statusCode: 200,
         body: JSON.stringify(doc),
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+              }
     };
 
   }
