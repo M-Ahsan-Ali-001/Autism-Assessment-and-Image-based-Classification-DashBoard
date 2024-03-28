bbc533b7958991292f4bfbedb12e1c53210b783b
Muhammad Ahsan Ali <91987110+M-Ahsan-Ali-001@users.noreply.github.com>
2024-03-28 12:01:46 -0700

    Update display_aq_10.js

--- netlify/functions/display_aq_10.js
+++ netlify/functions/display_aq_10.js
@@ -1,49 +1,36 @@
 const mongoose = require('mongoose');
+const { Employee } = require('../../backend/Schemas/aq_10_ins_schema.js');
 
-const {   Employee } =  require('../../backend/Schemas/aq_10_ins_schema.js');
 const mongoURI = "mongodb+srv://admin:admin123@atlascluster.f9crw3i.mongodb.net/usersDB";
 
 exports.handler = async function(event, context) {
-
-
-  
-
     await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-  try{
 
-    let id = JSON.parse(event.body).id
-    console.log("asdasdasd",id)
+    try {
+        let id = JSON.parse(event.body).id;
+        console.log("asdasdasd", id);
 
-    const doc =await Employee.collection.find({user_id:id}).toArray();
-      if(doc){
-        console.log("@*******",doc);
-
-
-  
-      }
-     
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify(doc),
-              headers: {
-                'Access-Control-Allow-Origin': '*',
-              }
-    };
-
-  }
-
-  catch(err){
-    return {
-        statusCode: 200,
-        body: err,
-        headers: {
-            'Access-Control-Allow-Origin': '*', // replace '*' with your origin
-            'Access-Control-Allow-Headers': 'Content-Type',
-    
+        const doc = await Employee.collection.find({ user_id: id }).toArray();
+        if (doc) {
+            console.log("@*******", doc);
         }
-    };
 
-  }
-  
-  };
+        return {
+            statusCode: 200,
+            body: JSON.stringify(doc),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            }
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: err.message }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            }
+        };
+    }
+};
