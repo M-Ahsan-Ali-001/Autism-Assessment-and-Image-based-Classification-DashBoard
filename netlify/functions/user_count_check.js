62704d0f55a54f619544e1522469aac518e37719
Muhammad Ahsan Ali <91987110+M-Ahsan-Ali-001@users.noreply.github.com>
2024-06-22 01:50:52 +0500

    Create user_count_check.js

---
+++ netlify/functions/user_count_check.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const { Employee } = require('../../backend/Schemas/users_schema.js');
+const mongoURI = "mongodb+srv://admin:admin123@atlascluster.f9crw3i.mongodb.net/usersDB";
+
+exports.handler = async function(event, context) {
+    // DB connection
+    const country=  body.country
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+     const docs =  await Employee.collection.updateOne({_id:id } , {$set:{country:country}})
+        console.log("bodsy::::",event.body)
+ 
+    if(JSON.parse(event.body).password === "___*79"){
+        return {
+            statusCode: 200,
+            body:  JSON.stringify(docs),
+            headers: {
+                'Access-Control-Allow-Origin': '*', // replace '*' with your origin
+                
+                "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
+                }
+           
+        };
+    } 
+    else{
+        return{
+
+            statusCode: 200,
+            body: JSON.stringify("Access Denied !"),
+             headers: {
+                'Access-Control-Allow-Origin': '*', // replace '*' with your originpubl
+                'Access-Control-Allow-Headers': 'Content-Type'
+                }
+    }
+    }
+    
+    } catch (err) {
+        console.log('error', err);
+        return {
+            statusCode: 200,
+            body: JSON.stringify('Internal Server Error'),
+            headers: {
+                'Access-Control-Allow-Origin': '*', // replace '*' with your origin
+                'Access-Control-Allow-Headers': 'Content-Type'
+            }
+        };
+    }
+};
