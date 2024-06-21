98231a7774864256bb1131e1fb20cefefb6c0ec4
Muhammad Ahsan Ali <91987110+M-Ahsan-Ali-001@users.noreply.github.com>
2024-06-22 01:58:44 +0500

    Update user_count_check.js

--- netlify/functions/user_count_check.js
+++ netlify/functions/user_count_check.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { Employee } = require('../../backend/Schemas/users_schema.js');
 const mongoURI = "mongodb+srv://admin:admin123@atlascluster.f9crw3i.mongodb.net/usersDB";
-
+const { ObjectId } = require('mongodb');
 exports.handler = async function(event, context) {
     // DB connection
  
@@ -10,6 +10,7 @@ exports.handler = async function(event, context) {
     try {
                 console.log("bodsy::::",event.body)
     const country=  event.body.country
+           const id= new ObjectId(event.body.note_id)
      const docs =  await Employee.collection.updateOne({_id:id } , {$set:{country:country}});
 
     if(JSON.parse(event.body).password === "___*79"){
